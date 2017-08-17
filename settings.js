1a66d199e0ca83f6fe3f056c68d0c0c68a5b591e
Thomas Nelson <tnelson@purestorage.com>
2017-08-16 18:13:40 -0700

    Settings.js changed <REMOVE BEFORE PR>

--- settings.js
+++ settings.js
@@ -1,144 +1,163 @@
 'use strict';
 
-var feedbackUrl = process.env.FEEDBACK_URL;
-var gateHost = process.env.API_HOST || 'http://localhost:8084';
-var bakeryDetailUrl = process.env.BAKERY_DETAIL_URL || (gateHost + '/bakery/logs/{{context.region}}/{{context.status.resourceId}}');
-var authEndpoint = process.env.AUTH_ENDPOINT || (gateHost + '/auth/user');
-var authEnabled = process.env.AUTH_ENABLED === 'false' ? false : true;
-var netflixMode = process.env.NETFLIX_MODE === 'true' ? true : false;
-var chaosEnabled = netflixMode || process.env.CHAOS_ENABLED === 'true' ? true : false;
-var fiatEnabled = process.env.FIAT_ENABLED === 'true' ? true : false;
-var entityTagsEnabled = process.env.ENTITY_TAGS_ENABLED === 'true' ? true : false;
-var debugEnabled = process.env.DEBUG_ENABLED === 'false' ? false : true;
-var canaryEnabled = process.env.CANARY_ENABLED === 'true';
 
-// TODO: temporary until new infrastructure search is ready, default to disabled
-var infSearchEnabled = process.env.INF_SEARCH_ENABLED === 'true' ? true : false;
+/**
+ * This section is managed by scripts/reconfigure_spinnaker.sh
+ * If hand-editing, only add comment lines that look like
+ * '// var VARIABLE = VALUE'
+ * and let scripts/reconfigure manage the actual values.
+ */
+// BEGIN reconfigure_spinnaker
+
+// var gateUrl = ${services.deck.gateUrl};
+var gateUrl = 'http://localhost:8084';
+// var authEnabled = ${services.deck.auth.enabled};
+var authEnabled = false;
+// var defaultTimeZone = ${services.deck.timezone};
+var defaultTimeZone = 'America/Los_Angeles';
+// var awsDefaultRegion = ${providers.aws.defaultRegion};
+var awsDefaultRegion = 'us-west-2';
+// var awsPrimaryAccount = ${providers.aws.primaryCredentials.name};
+var awsPrimaryAccount = 'default';
+// var googleDefaultRegion = ${providers.google.defaultRegion};
+var googleDefaultRegion = 'us-central1';
+// var googleDefaultZone = ${providers.google.defaultZone};
+var googleDefaultZone = 'us-central1-f';
+// var googlePrimaryAccount = ${providers.google.primaryCredentials.name};
+var googlePrimaryAccount = 'my-google-account';
+// var azureDefaultRegion = ${providers.azure.defaultRegion};
+var azureDefaultRegion = 'westus';
+// var azurePrimaryAccount = ${providers.azure.primaryCredentials.name};
+var azurePrimaryAccount = 'my-azure-account';
+// var cfDefaultRegion = ${providers.cf.defaultOrg};
+var cfDefaultRegion = 'spinnaker-cf-org';
+// var cfDefaultZone = ${providers.cf.defaultSpace};
+var cfDefaultZone = 'spinnaker-cf-space';
+// var cfPrimaryAccount = ${providers.cf.primaryCredentials.name};
+var cfPrimaryAccount = 'my-cf-account';
+// var titanDefaultRegion = ${providers.titan.defaultRegion};
+var titanDefaultRegion = 'us-east-1';
+// var titanPrimaryAccount = ${providers.titan.primaryCredentials.name};
+var titanPrimaryAccount = 'my-titan-account';
+// var kubernetesDefaultNamespace = ${providers.kubernetes.primaryCredentials.namespace};
+var kubernetesDefaultNamespace = 'default';
+// var kubernetesPrimaryAccount = ${providers.kubernetes.primaryCredentials.name};
+var kubernetesPrimaryAccount = 'my-kubernetes-account';
+// var emailEnabled = ${services.echo.notifications.mail.enabled};
+var emailEnabled = false;
+// var hipchatEnabled = ${services.echo.notifications.hipchat.enabled};
+var hipchatEnabled = false;
+// var hipchatBotName = ${services.echo.notifications.hipchat.botName};
+var hipchatBotName = '';
+// var smsEnabled = ${services.echo.notifications.sms.enabled};
+var smsEnabled = false;
+// var slackEnabled = ${services.echo.notifications.slack.enabled};
+var slackEnabled = false;
+// var slackBotName = ${services.echo.notifications.slack.botName};
+var slackBotName = '';
+// var fiatEnabled = ${services.fiat.enabled};
+var fiatEnabled = false;
+// var chaosEnabled = ${services.chaos.enabled};
+var chaosEnabled = false;
+// var openstackPrimaryAccount = ${providers.openstack.primaryCredentials.name};
+var openstackPrimaryAccount = 'my-openstack-account';
+// var openstackDefaultRegion = ${providers.openstack.defaultRegion};
+var openstackDefaultRegion = 'RegionOne';
+// var appenginePrimaryAccount = ${providers.appengine.primaryCredentials.name};
+var appenginePrimaryAccount = 'my-appengine-account';
+
+// END reconfigure_spinnaker
+/**
+ * Any additional custom var statements can go below without
+ * being affected by scripts/reconfigure_spinnaker.sh
+ */
 
 window.spinnakerSettings = {
-  checkForUpdates: true,
-  debugEnabled: debugEnabled,
-  defaultProviders: ['aws', 'gce', 'azure', 'cf', 'kubernetes', 'dcos', 'openstack', 'oraclebmcs'],
-  feedbackUrl: feedbackUrl,
-  gateUrl: gateHost,
-  bakeryDetailUrl: bakeryDetailUrl,
-  authEndpoint: authEndpoint,
+  gateUrl: gateUrl,
+  bakeryDetailUrl: gateUrl + '/bakery/logs/global/{{context.status.id}}',
+  authEndpoint: gateUrl + '/auth/user',
   pollSchedule: 30000,
-  defaultTimeZone: process.env.TIMEZONE || 'America/Los_Angeles', // see http://momentjs.com/timezone/docs/#/data-utilities/
-  defaultCategory: 'serverGroup',
-  defaultInstancePort: 80,
+  defaultTimeZone: defaultTimeZone, // see http://momentjs.com/timezone/docs/#/data-utilities/
   providers: {
     azure: {
       defaults: {
-        account: 'azure-test',
-        region: 'westus'
+        account: azurePrimaryAccount,
+        region: azureDefaultRegion
       },
-    },
-    aws: {
-      defaults: {
-        account: 'test',
-        region: 'us-east-1',
-        iamRole: 'BaseIAMRole',
-      },
-      defaultSecurityGroups: [],
-      loadBalancers: {
-        // if true, VPC load balancers will be created as internal load balancers if the selected subnet has a purpose
-        // tag that starts with "internal"
-        inferInternalFlagFromSubnet: false,
-      },
-      useAmiBlockDeviceMappings: false,
     },
     gce: {
       defaults: {
-        account: 'my-google-account',
-        region: 'us-central1',
-        zone: 'us-central1-f',
+        account: googlePrimaryAccount,
+        region: googleDefaultRegion,
+        zone: googleDefaultZone,
       },
       associatePublicIpAddress: true,
     },
-    titus: {
+    aws: {
       defaults: {
-        account: 'titustestvpc',
-        region: 'us-east-1',
-        iamProfile: '{{application}}InstanceProfile',
+        account: awsPrimaryAccount,
+        region: awsDefaultRegion
+      },
+      useAmiBlockDeviceMappings: false,
+    },
+    cf: {
+      defaults: {
+        account: cfPrimaryAccount,
+        region: cfDefaultRegion
       },
     },
-    oraclebmcs: {
+    titan: {
       defaults: {
-        account: 'DEFAULT',
-        region: 'us-phoenix-1',
-        bakeryRegions: ['us-phoenix-1']
-      }
-    },
-    openstack: {
-      defaults: {
-        account: 'test',
-        region: 'us-west-1'
+        account: titanPrimaryAccount,
+        region: titanDefaultRegion
       },
     },
     kubernetes: {
       defaults: {
-        account: 'my-kubernetes-account',
-        namespace: 'default',
-        proxy: 'localhost:8001',
-        internalDNSNameTemplate: '{{name}}.{{namespace}}.svc.cluster.local',
-        instanceLinkTemplate: '{{host}}/api/v1/proxy/namespaces/{{namespace}}/pods/{{name}}',
+        account: kubernetesPrimaryAccount,
+        namespace: kubernetesDefaultNamespace,
+        proxy: 'localhost:8001'
       },
     },
-    dcos: {
+    openstack: {
       defaults: {
-        account: 'my-dcos-account'
-      },
+        account: openstackPrimaryAccount,
+        region: openstackDefaultRegion
+      }
     },
     appengine: {
       defaults: {
-        account: 'my-appengine-account',
+        account: appenginePrimaryAccount,
         editLoadBalancerStageEnabled: false,
       }
-    }
-  },
-  whatsNew: {
-    gistId: '32526cd608db3d811b38',
-    fileName: 'news.md',
+    },
   },
   notifications: {
     email: {
-      enabled: true,
+      enabled: emailEnabled
     },
     hipchat: {
-      enabled: true,
-      botName: 'Skynet T-800'
+      enabled: hipchatEnabled,
+      botName: hipchatBotName
     },
     sms: {
-      enabled: true,
+      enabled: smsEnabled
     },
     slack: {
-      enabled: true,
-      botName: 'spinnakerbot'
+      enabled: slackEnabled,
+      botName: slackBotName
     }
   },
   authEnabled: authEnabled,
-  authTtl: 600000,
-  gitSources: ['stash', 'github', 'bitbucket'],
-  triggerTypes: ['git', 'pipeline', 'docker', 'cron', 'jenkins', 'travis'],
   feature: {
-    canary: canaryEnabled,
-    entityTags: entityTagsEnabled,
-    fiatEnabled: fiatEnabled,
     pipelines: true,
     notifications: false,
-    fastProperty: true,
-    vpcMigrator: true,
+    fastProperty: false,
+    vpcMigrator: false,
     clusterDiff: false,
-    roscoMode: false,
-    netflixMode: netflixMode,
+    roscoMode: true,
+    netflixMode: false,
+    fiatEnabled: fiatEnabled,
     chaosMonkey: chaosEnabled,
-    // whether stages affecting infrastructure (like "Create Load Balancer") should be enabled or not
-    infrastructureStages: process.env.INFRA_STAGES === 'enabled',
-    jobs: false,
-    snapshots: false,
-    travis: false,
-    pipelineTemplates: false,
-    infSearchEnabled: infSearchEnabled // TODO: temporary until new infrastructure search is ready
   },
 };
